63f665f6612642287c50b5db00de046cb0a1d1e8
André Abadesso <andre.abadesso@gmail.com>
2015-06-28 13:23:17 -0300

    feat(parser): added the option to filter geometries returned by the KML parser, added tests

--- tests/test.js
+++ tests/test.js
@@ -4,20 +4,20 @@ var should = chai.should();
 describe('KML Parsing', function() {
 
     it('Should parse a KML and return an object', function() {
-        expect(typeof KMLParser.loadKML(kmlString)).to.be.equal('object');
+        expect(typeof KMLParser.loadKML(kmlString[0])).to.be.equal('object');
     });
 
     it('Should have head and body as keys on the Parsed KML', function() {
-        expect(KMLParser.loadKML(kmlString)).to.have.all.keys(['head', 'body']);
+        expect(KMLParser.loadKML(kmlString[0])).to.have.all.keys(['head', 'body']);
     });
 
     it('Should have a geojson value on head values', function() {
-        expect(KMLParser.loadKML(kmlString).head).to.include.any.members(['geojson']);
+        expect(KMLParser.loadKML(kmlString[0]).head).to.include.any.members(['geojson']);
     });
 
     // TODO: Find a better way to do this:
     it('Should have the same number of members on head and members of body', function() {
-        var parsed = KMLParser.loadKML(kmlString);
+        var parsed = KMLParser.loadKML(kmlString[0]);
         var sameNumber = true;
 
         parsed.body.forEach(function(bodyItem) {
@@ -27,6 +27,105 @@ describe('KML Parsing', function() {
         });
 
         expect(sameNumber).to.be.equal(true);
+    });
+
+
+    it('Should ignore geometies that aren\'t points when point parameter is passed', function() {
+        var parsed = KMLParser.loadKML(kmlString[1], 'Point');
+        var index = parsed.head.indexOf('geojson');
+
+        var geometry = _.chain(parsed.body)
+                            .map(function(geo) {
+                                return JSON.parse(geo[index]);
+                            })
+                            .filter(function(point) {
+                                return point.type !== 'Point';
+                            })
+                            .value();
+
+        expect(geometry).to.have.length.below(1);
+    });
+
+    it('Should ignore geometies that aren\'t linestrings when linestring parameter is passed', function() {
+
+        var parsed = KMLParser.loadKML(kmlString[1], 'LineString');
+        var index = parsed.head.indexOf('geojson');
+
+        var geometry = _.chain(parsed.body)
+                            .map(function(geo) {
+                                return JSON.parse(geo[index]);
+                            })
+                            .filter(function(point) {
+                                return point.type !== 'LineString';
+                            })
+                            .value();
+
+        expect(geometry).to.have.length.below(1);
+    });
+
+    it('Should ignore geometies that aren\'t Polygons when linestring parameter is passed', function() {
+
+        var parsed = KMLParser.loadKML(kmlString[1], 'Polygon');
+        var index = parsed.head.indexOf('geojson');
+
+        var geometry = _.chain(parsed.body)
+                            .map(function(geo) {
+                                return JSON.parse(geo[index]);
+                            })
+                            .filter(function(point) {
+                                return point.type !== 'Polygon';
+                            })
+                            .value();
+
+        expect(geometry).to.have.length.below(1);
+    });
+
+    it('Should ignore parse a KML with more than one type of geometry and return only points', function() {
+        var parsed = KMLParser.loadKML(kmlString[1], 'Point');
+        var index = parsed.head.indexOf('geojson');
+
+        var geometry = _.chain(parsed.body)
+                            .map(function(geo) {
+                                return JSON.parse(geo[index]);
+                            })
+                            .filter(function(point) {
+                                return point.type === 'Point';
+                            })
+                            .value();
+
+        expect(geometry).to.have.length.above(0);
+    });
+
+    it('Should ignore parse a KML with more than one type of geometry and return only LineStrings', function() {
+        var parsed = KMLParser.loadKML(kmlString[1], 'LineString');
+        var index = parsed.head.indexOf('geojson');
+
+        var geometry = _.chain(parsed.body)
+                            .map(function(geo) {
+                                return JSON.parse(geo[index]);
+                            })
+                            .filter(function(point) {
+                                return point.type === 'LineString';
+                            })
+                            .value();
+
+        expect(geometry).to.have.length.above(0);
+    });
+
+    it('Should ignore parse a KML with more than one type of geometry and return only Polygons', function() {
+        var parsed = KMLParser.loadKML(kmlString[1], 'Polygon');
+        var index = parsed.head.indexOf('geojson');
+
+        var geometry = _.chain(parsed.body)
+                            .map(function(geo) {
+                                return JSON.parse(geo[index]);
+                            })
+                            .filter(function(point) {
+                                return point.type === 'Polygon';
+                            })
+                            .value();
+
+        expect(geometry).to.have.length.above(0);
     });
 
 });
